dd60dc1ee0115cdc7a9af9e742f4104f497edfbe
liuboringbook <1287128847@qq.com>
2019-07-12 00:00:51 +0800

    初始化和首屏

---
+++ js/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by 刘如刚 on 2019/7/11.
+ */
+$(function(){
+    /*初始化full组件 */
+    $('#dowebok').fullpage({
+        /*配置参数*/
+        /*背景颜色*/
+        sectionsColor:["#fadd67","#84a2d4","#ef674d","#ffeedd","#d04759","#84d9ed","#8ac060"],
+        /*设置屏幕内容的对齐方式*/
+        verticalCentered:  false,
+        /*设置导航，设置指示器*/
+        navigation:true
+    });
+})
