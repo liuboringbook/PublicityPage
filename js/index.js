c16f069aa595e7e8248bbbacd1e3d491485e2a54
liuboringbook <1287128847@qq.com>
2019-07-13 00:51:58 +0800

    第3屏和第四屏

--- js/index.js
+++ js/index.js
@@ -5,6 +5,8 @@ $(function(){
     /*初始化full组件 */
     $('#dowebok').fullpage({
         /*配置参数*/
+        /*页面切换时间*/
+        scrollingSpeed: 1000,
         /*背景颜色*/
         sectionsColor:["#fadd67","#84a2d4","#ef674d","#ffeedd","#d04759","#84d9ed","#8ac060"],
         /*设置屏幕内容的对齐方式*/
@@ -16,7 +18,27 @@ $(function(){
            setTimeout(function(){
                $('.section').eq(index-1).addClass('now')
            })
-
+        },
+        /*离开某个页面的时候触发*/
+        onLeave: function(index,nextIndex,direction){
+            if(index == 2 && nextIndex == 3){
+                /*当前是从第二页到第三页*/
+                $('.section').eq(index-1).addClass('leaved')
+            }
+        },
+        afterRender: function(){
+            // console.log(this);//$('#dowebok')
+            /*this没有api方法*/
+            /*jquery插件初始化封装这个方法*/
+            /*1,回想jquery插件的封装，$.fn.fullpage = function(){}*/
+            /*2.jquery本身没有的方法通过$.fn的方式追加方法，认为是插件方法*/
+            /*3,例如：$.fn.src = function(){return this.attr('src')} this 你选择谁this(jquery对象)执行谁*/
+            $('.more').on('click',function(){
+                $.fn.fullpage.moveSectionDown()
+            })
         }
+        /*点击更多切换下一页*/
+        /*最好在组件出示完毕或者插件渲染完毕*/
+
     });
 })
